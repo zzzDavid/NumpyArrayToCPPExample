ed8b998968d878ebfd603ab75e68c07a0efbd4e4
Niansong Zhang <nz264@cornell.edu>
2022-07-25 15:40:42 -0400

    use 32-bit example

--- main.cpp
+++ main.cpp
@@ -3,20 +3,20 @@
 
 int main() {
 
-    int8_t array[4][4][5][5];
+    int32_t array[4][4][5][5];
 
     FILE* fp;
     fp = fopen("./array.bin", "rb");
     
-    auto buff = new unsigned char[4*4*5*5];
+    auto buff = new unsigned char[4*4*5*5*4];
 
-    fread(buff, 1, 4*4*5*5, fp);
+    fread(buff, 1, 4*4*5*5*4, fp);
 
     for (int i0 = 0; i0 < 4; i0++) {
         for (int i1 = 0; i1 < 4; i1++) {
             for (int i2 = 0; i2 < 5; i2++) {
                 for (int i3 = 0; i3 < 5; i3++) {
-                    array[i0][i1][i2][i3] = (int8_t) buff[i0 * 4  + i1 * 5 + i2 * 5 + i3];
+                    array[i0][i1][i2][i3] = (int32_t) buff[4 * (i0 * 4 * 5 * 5  + i1 * 5 * 5 + i2 * 5 + i3)];
                 }
             }
         }
@@ -27,7 +27,7 @@ int main() {
             std::cout << "[ ";
             for (int i2 = 0; i2 < 5; i2++) {
                 for (int i3 = 0; i3 < 5; i3++) {
-                    std::cout << static_cast<int16_t>( array[i0][i1][i2][i3] ) << ", ";
+                    std::cout << array[i0][i1][i2][i3] << ", ";
                 }
                 std::cout << "\n";
             }
--- output_4d_array.py
+++ output_4d_array.py
@@ -2,7 +2,8 @@ import numpy as np
 
 a = np.random.rand(4, 4, 5, 5) * 100
 
-a = a.astype(np.int8)
+a = a.astype(np.int32)
+# a = a.astype(np.int8)
 print(a)
 filename = "array.bin"
 a.tofile(filename)
